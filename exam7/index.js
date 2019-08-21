4387dc1eba9fa151c6c2946508a78d04a4888511
zhuwei <zhuwei@koolearn-inc.com>
2019-08-21 17:25:45 +0800

    完成平行光 + 环境光测试

--- exam7/index.js
+++ exam7/index.js
@@ -35,8 +35,8 @@ function main() {
         0,0,-1,  0,0,-1,  0,0,-1,  0,0,-1,  0,0,-1,  0,0,-1,
         0,1,0,  0,1,0,  0,1,0,  0,1,0,  0,1,0,  0,1,0,
         0,-1,0,  0,-1,0,  0,-1,0,  0,-1,0,  0,-1,0,  0,-1,0,
-        1,0,0,  1,0,0,  1,0,0,  1,0,0,  1,0,0,  1,0,0,
         -1,0,0,  -1,0,0,  -1,0,0,  -1,0,0,  -1,0,0,  -1,0,0,
+        1,0,0,  1,0,0,  1,0,0,  1,0,0,  1,0,0,  1,0,0,
     ])
     var red = [200,0,0] ,green = [0,200,0], blue = [0,0,200]
     var colors = new Uint8Array([
@@ -57,69 +57,59 @@ function main() {
     var program = util.createProgramFromScripts(gl,['vertex','fragment'])
     gl.useProgram(program)
 
-    var poBuffer = gl.createBuffer()
-    var colorBuffer = gl.createBuffer()
-    var direcBuffer = gl.createBuffer()
-
-    var u_matrix_loc = gl.getUniformLocation(program, 'u_matrix')
     var mat = new Matrix4();
     var viewMat = new Matrix4();
     var projectMat = new Matrix4();
 
-    var fovy = 30,aspect=1,near=3,far = 100;
+    // 相机
+    var fovy = 30,aspect=1,near=1,far = 100;
     // projectMat.setOrtho(-1,1,-1,1,-1,4)
     projectMat.setPerspective(fovy,aspect,near,far)
-    viewMat.setLookAt(5,7, 1, 0,0,0, 0,1,0);
+    viewMat.setLookAt(3,3, 7, 0,0,0, 0,1,0);
     mat.set(projectMat).multiply(viewMat)
+    var u_matrix_loc = gl.getUniformLocation(program, 'u_matrix')
     gl.uniformMatrix4fv(u_matrix_loc, false, mat.elements)
 
-    var u_light = gl.getUniformLocation(program, 'u_light')
-    var vec3 = new Vector3([1,0,0]);
-    vec3.normalize();
+    var normalMat = new Matrix4();
+    normalMat.setIdentity();
+    var u_normal_mat = gl.getUniformLocation(program, 'u_normal_mat');
+    gl.uniformMatrix4fv(u_normal_mat, false, normalMat.elements)
 
+    // 光线
+    var vec3 = new Vector3([5,10.0,6.0]);
+    vec3.normalize();
+    var u_light = gl.getUniformLocation(program, 'u_parallel_light_direction')
     gl.uniform3fv(u_light,vec3.elements)
 
-    gl.bindBuffer(gl.ARRAY_BUFFER,poBuffer)
-    gl.bufferData(gl.ARRAY_BUFFER, points, gl.STATIC_DRAW)
-    var a_position_loc = gl.getAttribLocation(program,'a_position')
-    gl.enableVertexAttribArray(a_position_loc);
-    var size = 3;          // 每次迭代运行提取两个单位数据
-    var type = gl.FLOAT;   // 每个单位的数据类型是32位浮点型
-    var normalize = false; // 不需要归一化数据
-    var stride = 0;        // 0 = 移动单位数量 * 每个单位占用内存（sizeof(type)）
-    var offset = 0;        // 从缓冲起始位置开始读取
-    gl.vertexAttribPointer(
-        a_position_loc, size, type, normalize, stride, offset)
+    var u_parallel_light_color = gl.getUniformLocation(program, 'u_parallel_light_color')
+    gl.uniform3fv(u_parallel_light_color,new Float32Array([1.0,1.0,1.0]))
 
-    gl.bindBuffer(gl.ARRAY_BUFFER,colorBuffer)
-    gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW)
-    var a_color_loc = gl.getAttribLocation(program,'a_color')
-    gl.enableVertexAttribArray(a_color_loc);
-    var size1 = 3;          // 每次迭代运行提取两个单位数据
-    var type1 = gl.UNSIGNED_BYTE;   // 每个单位的数据类型是32位浮点型
-    var normalize1 = true; // 不需要归一化数据
-    var stride1 = 0;        // 0 = 移动单位数量 * 每个单位占用内存（sizeof(type)）
-    var offset1 = 0;        // 从缓冲起始位置开始读取
-    gl.vertexAttribPointer(
-        a_color_loc, size1, type1, normalize1, stride1, offset1)
+    var u_environment_light_color = gl.getUniformLocation(program, 'u_environment_light_color')
+    gl.uniform3fv(u_environment_light_color,new Float32Array([0.2,0.2,0.2]))
 
-    gl.bindBuffer(gl.ARRAY_BUFFER,direcBuffer)
-    gl.bufferData(gl.ARRAY_BUFFER, directions, gl.STATIC_DRAW)
-    var a_direction_loc = gl.getAttribLocation(program,'a_direction')
-    gl.enableVertexAttribArray(a_direction_loc);
-    var size2 = 3;          // 每次迭代运行提取两个单位数据
-    var type2 = gl.FLOAT;   // 每个单位的数据类型是32位浮点型
-    var normalize2 = true; // 不需要归一化数据
-    var stride2 = 0;        // 0 = 移动单位数量 * 每个单位占用内存（sizeof(type)）
-    var offset2 = 0;        // 从缓冲起始位置开始读取
-    gl.vertexAttribPointer(
-        a_direction_loc, size2, type2, normalize2, stride2, offset2)
+    bindArrayBuffer(points,program,'a_position',gl.FLOAT,false);
+    bindArrayBuffer(colors,program,'a_color',gl.UNSIGNED_BYTE,true);
+    bindArrayBuffer(directions,program,'a_direction',gl.FLOAT,false);
 
     gl.enable(gl.DEPTH_TEST)
     gl.clearColor(1, 1, 1, 1)
     gl.clear(gl.COLOR_BUFFER_BIT)
 
     gl.drawArrays(gl.TRIANGLES, 0, 36)
+
+    function bindArrayBuffer(data,program,attribute,type,normal) {
+        var buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER,buffer)
+        gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW)
+        var a_loc = gl.getAttribLocation(program,attribute)
+        gl.enableVertexAttribArray(a_loc);
+        var size = 3;          // 每次迭代运行提取两个单位数据
+        var dataType = type || gl.FLOAT;   // 每个单位的数据类型是32位浮点型
+        var normalize = normal; // 不需要归一化数据
+        var stride = 0;        // 0 = 移动单位数量 * 每个单位占用内存（sizeof(type)）
+        var offset = 0;        // 从缓冲起始位置开始读取
+        gl.vertexAttribPointer(a_loc, size, dataType, normalize, stride, offset)
+    }
 
     var then = 0;
     var speed = 2;
@@ -152,11 +142,21 @@ function main() {
         var mat = new Matrix4();
         var viewMat = new Matrix4();
         var projectMat = new Matrix4();
+        var normal = new Matrix4();
+        var model = new Matrix4();
+
+        // model.setIdentity()
+        model.setRotate(z,0,1,0)
         // projectMat.setOrtho(-1,1,-1,1,8,100)
         projectMat.setPerspective(fovy,aspect,near,far)
-        viewMat.setLookAt(x,y,z, 0,0,0, 0,1,0);
-        mat.set(projectMat).multiply(viewMat)
-        gl.uniformMatrix4fv(u_matrix_loc, false, mat.elements)
+        viewMat.setLookAt(x,y,7, 0,0,0, 0,1,0);
+        mat.set(projectMat).multiply(viewMat).multiply(model)
+        gl.uniformMatrix4fv(u_matrix_loc, false, mat.elements);
+
+        normal.setInverseOf(model);
+        normal.transpose();
+        gl.uniformMatrix4fv(u_normal_mat, false, normal.elements);
+
         gl.drawArrays(gl.TRIANGLES, 0, 36)
     }
 
